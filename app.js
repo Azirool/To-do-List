c83e385ecdf57f3b981ae001a732aeafe453650a
Azirool Fareed <aziroolfareed31@gmail.com>
2023-08-30 20:47:45 +0800

    adding css to the project

--- app.js
+++ app.js
@@ -2,31 +2,34 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-var items = ['Buy Food', 'Eat Food', 'Cook Food'];
+let items = ["Buy Food", "Eat Food", "Cook Food"];
 
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
 app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
 
-    var currentDate = new Date();
-    var options = {
+    let currentDate = new Date();
+
+    let options = {
         weekday: "long",
         day: "numeric",
         month: "long"
     }
-    var day = currentDate.toLocaleDateString("en-US", options);
+
+    let day = currentDate.toLocaleDateString("en-US", options);
 
     res.render('list', {todayDate: day, newItems: items});
 });
 
 app.post("/", (request, response) => {
-    var item = request.body.todoInput;
-
+    let item = request.body.todoInput;
     items.push(item);
 
     response.redirect("/");
-})
+});
 
 app.listen(3000, () => {
     console.log("Server is Running");
