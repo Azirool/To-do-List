d224bc7f7b78a330a1e78ba7ae4ccfa5bae18404
Azirool Fareed <aziroolfareed31@gmail.com>
2023-09-04 14:44:44 +0800

    Implement MongoDB using Mongoose and added comment to the code

--- app.js
+++ app.js
@@ -1,46 +1,146 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js")
+const mongoose = require("mongoose");
 
+//Express
 const app = express();
-const items = ["Buy Food", "Eat Food", "Cook Food"];
-const workItems = [];
 
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static("public"));
-
+//EJS
 app.set('view engine', 'ejs');
 
+//Body-parser
+app.use(bodyParser.urlencoded({extended:true}));
+//Express
+app.use(express.static("public"));
+
+//Mongoose connection
+mongoose.connect("mongodb://127.0.0.1:27017/todolistDB");
+
+//Schema for the list item
+const itemsSchema = new mongoose.Schema({
+    name: String
+});
+
+//Collection/Table for MongoDB
+const Item = mongoose.model("Item", itemsSchema);
+
+//Default data for the database
+const item1 = new Item({
+    name: "Welcome to your todolist!"
+});
+
+const item2 = new Item({
+    name: "Hit the + button to add a new item."
+});
+
+const item3 = new Item({
+    name: "<-- Hit this to delete an item."
+});
+
+const defaultItem = [item1, item2, item3];
+
+//Schema for every new page created
+const listSchema = {
+    name: String,
+    items: [itemsSchema]
+};
+
+//Collection/table for MongoDB
+const List = mongoose.model("List", listSchema);
+
+//Home Route
 app.get("/", (req, res) => {
+    //Date JS
     let day = date();
-
-    res.render('list', {listTitle: day, newItems: items});
+    //Read data from MongoDB using Mongoose
+    Item.find({}).then((foundItems) => {
+        if(foundItems.length === 0){
+            //Insert default item
+            Item.insertMany(defaultItem);
+            //Redirect page
+            res.redirect("/");
+        } else{
+            //Render page with the data from MongoDB
+            res.render('list', {listTitle: day, newItems: foundItems});
+        }
+    });
 });
 
-app.get("/work", (req, res) => {
-    res.render('list', {listTitle: "Work List", newItems:workItems});
-})
+//Custom Route
+app.get("/:customListPage", (req,res) => {
+    //Store what user type in the search bar
+    const customListPage = req.params.customListPage;
 
-app.post("/", (request, response) => {
-    const item = request.body.todoInput;
-    if(request.body.list === "Work"){
-        workItems.push(item);
-        response.redirect("/work");
-    } else {
-        items.push(item);
-        response.redirect("/");
+    //Check for existing data
+    List.findOne({name:customListPage}).then((result) =>{
+        if (result){
+            //Show existing list
+            console.log("Already Existed");
+            //Render page
+            res.render('list', {listTitle: result.name, newItems: result.items});
+        } else {
+            //Create new list
+            const list = new List({
+                name: customListPage,
+                items: defaultItem
+            });
+            //Save data into MongoDB
+            list.save();
+            //Redirect page after saving data into the database
+            res.redirect("/" + customListPage);
+        }
+    });
+});
+
+//Handle post request from HTML file
+app.post("/", (req, res) => {
+    //Date JS
+    let day = date();
+    //Store input from HTML textbox input
+    const itemName = req.body.todoInput;
+    //Store input from HTML buttom
+    const listName = req.body.list;
+    //Insert input from HTML file into MongoDB
+    const newItem = new Item({
+        name: itemName
+    });
+
+    //Check which collection/table to store data
+    if (listName === day){
+        //Save data into MongoDB
+        newItem.save();
+        //Redirect Page
+        res.redirect("/");
+    } else{
+        //Find which collection/table user want to access
+        List.findOne({name:listName}).then((foundList) => {
+            //Push new data into MongoDB
+            foundList.items.push(newItem);
+            //Save data into MongoDB
+            foundList.save();
+            //Redirect Page
+            res.redirect("/" + listName);
+        });
     }
-   
-    response.redirect("/");
 });
 
-app.post("/work", (req, res) => {
-    const item = res.body.todoInput;
-    workItems.push(item);
-    
-    res.redirect("/work");
-})
+//Handle post request from HTML file
+app.post("/delete", (req,res) => {
+    //Store data from HTML checkbox input
+    const checkItem = req.body.checkbox;
+    //Search and delete data based on the HTML checkbox input
+    Item.findByIdAndRemove(checkItem).exec().then(() => {
+        console.log("Data deleted");
+        //Redirect page
+        res.redirect("/");
+    }).catch((err) => {
+        //Show error
+        console.log(err);
+    });
+});
 
+//Start express server
 app.listen(3000, () => {
     console.log("Server is Running");
 });
